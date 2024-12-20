8eaffdb4d2042f8af0adcf6b6fbc596deebba001
theo-al <emailtheoprauerj@gmail.com>
2024-12-19 23:40:32 -0300

    menu!

--- cliente/main.py
+++ cliente/main.py
@@ -1,4 +1,4 @@
-from xmlrpc.client import ServerProxy
+from xmlrpc.client import ServerProxy, Fault
 
 from threading import Thread
 from queue     import Queue
@@ -10,20 +10,17 @@ from .. import BIND_ADDR, BIND_PORT
 
 
 #! lidar com entrada inválida pro rpc
-#! fazer menu principal (com opção de ver e criar salas)
-#! fazer uma thread buscadora de mensagem
 
-#! ver se tem que lidar com Faults
 #! mostrar diferente quando alguém entra na sala
 #! parsear args linha de comando
 #! fazer funçãozinha pra lidar com line wrap e linhas
-#! cores
+#! cores (+- já)
 
 
 # setape
-binder = ServerProxy(f"http://{BIND_ADDR}:{BIND_PORT}", allow_none=True) #! binder
+binder = ServerProxy(f"http://{BIND_ADDR}:{BIND_PORT}", allow_none=True)
 CHAT_ADDR, CHAT_PORT = binder.get_addr()
-server = ServerProxy(f"http://{CHAT_ADDR}:{CHAT_PORT}", allow_none=True) #! binder
+server = ServerProxy(f"http://{CHAT_ADDR}:{CHAT_PORT}", allow_none=True)
 
 username  = None
 room_name = None
@@ -31,7 +28,7 @@ room_name = None
 msgs  = list[dict]()
 q     = Queue[str]()
 
-ui.input.queue = q #! explicar
+ui.input.queue = q # explicar no vídeo
 
 def connected(): #! colocar no stub do binder aqui do client
     try: server.ping()
@@ -40,26 +37,118 @@ def connected(): #! colocar no stub do binder aqui do client
     except Exception as e:  return False #! checar
     else:                   return True
 
+def menu(sel: int = 0) -> bool:
+    global username, room_name
+
+    options = [
+        'iniciar',
+        'lista de salas',
+        'escolher nome',
+        'escolher sala',
+        'sair',
+    ]
+    while True:
+        ui.clear_scr()
+        menu_screen(options, sel)
+
+        cmd = q.get()
+        if not cmd: break
+
+        for c in cmd:
+            if c == "n": sel += 1
+            if c == "p": sel -= 1
+            sel %= len(options)
+
+    selected = options[sel]
+    if selected == 'iniciar': return True
+    if selected == 'sair':    raise KeyboardInterrupt
+
+    if   selected == 'lista de salas':
+        ui.clear_scr()
+        ui.input(title='salas disponíveis:',
+                 lines=server.list_rooms(),
+                 prompt="aperte enter para sair")
+    elif selected == 'escolher nome':
+        ui.clear_scr()
+        username = ui.input("insira seu nome de usuário")
+    elif selected == 'escolher sala':
+        ui.clear_scr()
+        room_name = ui.input(title='salas disponíveis:',
+                             prompt="insira nome da sala",
+                             lines=server.list_rooms())
+
+    return False
+        
+def menu_screen(options, selection: int):
+    selected = options[selection]
+    opt = options.copy() + ['']
+    opt[selection] = ui.highlight(selected)
+    ui.draw_scr(title="menu",
+                lines=opt,
+                extra="n/p <enter> para se mover e <enter> para selecionar",
+                prompt='')
+
+def register() -> tuple[str, str]:
+    ui.clear_scr()
+    _username  = username  if username  else \
+                 ui.input("insira seu nome de usuário")
+    _room_name = room_name if room_name else \
+                 ui.input(title='salas disponíveis:',
+                          prompt="insira nome da sala",
+                          lines=server.list_rooms())
+    return _username, _room_name
+
+def join(room_name): #! dar uma mudada
+    global username
+
+    ok, err = server.join_room(username, room_name)
+    if not ok: #! fazer match
+        if   err == 'room':
+            ans = ui.input(title="sala inexistente.",
+                           prompt=f"deseja criar a nova sala {room_name}? (s/n)") \
+                          .strip().casefold()
+            if   ans == 's' or ans == 'y' or not ans:
+                ok, err = server.create_room(room_name)
+                ok, err = server.join_room(username, room_name)
+            elif ans == 'n':
+                return False, ui.draw_scr(extra="ok", prompt='')
+            else:
+                return False, ui.draw_scr(extra="por favor tente novamente", prompt='')
+        elif err == 'user':
+            username = ui.input(title='erro: nome de usuário já em uso.',
+                                prompt="digite um novo nome:")
+            ok, err = server.join_room(username, room_name)
+
+    if ok: msgs = err
+    else:
+        ui.draw_scr(title="ocorreu um erro inesperado",
+                    extra="por favor tente novamente")
+        return False, ()
+    
+    return True, msgs
+
+
 def is_cmd(s: str, cmd: str) -> bool:
     s = s.strip().casefold()
     return cmd.startswith(s)
 
-def queue_input(queue: Queue) -> Never:
-    while True:
-        msg = input().strip()
-        queue.put(msg)
+def interpret_command(msg: str, server=server) -> tuple[list, str]: #! melhorar
+    global msgs, username, room_name #!
 
-def interpret_command(msg: str,) -> tuple[list, str]:
     lines, extra = [], ' '
-    if len(msg) > 1:
-        cmd, *args = msg[1:].split()
-    else: return lines, extra
+    if len(msg) <= 1: return lines, extra
+
+    cmd, *args = msg[1:].split()
 
     if   is_cmd(cmd, 'quit'): raise KeyboardInterrupt # rsrsrs
-    elif is_cmd(cmd, 'exit'):
-        raise KeyboardInterrupt
-        #! tamo fazendo igual o quit, sair da sala levar pra um menu em vez disso
+    elif is_cmd(cmd, 'exit'): #!
+        server.leave_room(username, room_name)
+        while not menu(): pass
 
+        username, room_name = register()
+        ok, msgs = join(room_name)
+        if not ok:
+            raise KeyboardInterrupt
     elif is_cmd(cmd, 'tell'):
         usage = "modo de uso: tell <destino> <mensagem>"
         if args:
@@ -86,48 +175,20 @@ def interpret_command(msg: str,) -> tuple[list, str]:
 
     return lines, extra
 
-def register_menu(): #! tá meio esquisito
-    global username, room_name
-
-    ui.clear_scr()
-    username  = username  if username  else \
-                ui.input("insira seu nome de usuário")
-    room_name = room_name if room_name else \
-                ui.input(title='salas disponíveis:',
-                         prompt="insira nome da sala",
-                         lines=server.list_rooms())
-
-    ok, err = server.join_room(username, room_name)
-    if not ok: #! fazer match
-        if   err == 'room':
-            ans = ui.input(title="erro: sala inexistente.",
-                           prompt=f"deseja criar a nova sala {room_name}? (s/n)") \
-                          .strip().casefold()
-            if   ans == 's' or ans == 'y' or not ans:
-                ok, err = server.create_room(room_name)
-                ok, err = server.join_room(username, room_name)
-            elif ans == 'n':
-                return False, ui.draw_scr(extra="ok", prompt='')
-            else:
-                return False, ui.draw_scr(extra="por favor tente novamente", prompt='')
-        elif err == 'user':
-            username = ui.input(title='erro: nome de usuário já escolhido.',
-                                prompt="digite um novo nome:")
-            ok, err = server.join_room(username, room_name)
-
-    if ok: msgs = err
-    else:
-        ui.draw_scr(title="ocorreu um erro inesperado",
-                    extra="por favor tente novamente")
-        return False, ()
-    
-    return True, msgs
+def queue_input(queue: Queue) -> Never:
+    while True:
+        msg = input().strip()
+        queue.put(msg)
 
 def main():
-    global msgs
+    global msgs, username, room_name
 
-    ok, msgs = register_menu()
-    if not ok: raise KeyboardInterrupt
+    while not menu(): pass
+
+    username, room_name = register()
+    ok, msgs = join(room_name)
+    if not ok:
+        raise KeyboardInterrupt
 
     clear = True
     lines = []
@@ -172,6 +233,9 @@ if __name__ == "__main__":
         ui.clear_scr() #! talvez perguntar se tentar de novo
         ui.warn("erro na comunicação com o servidor")
         ui.warn("saindo do programa")
+    except Fault as e:
+        ui.warn(f"erro {e} no servidor")
+        ui.warn("saindo do programa") #! não sair
     except KeyboardInterrupt:
         ui.warn("saindo do programa")
     finally:
--- cliente/ui.py
+++ cliente/ui.py
@@ -16,8 +16,8 @@ else:
     def clear_scr():
         print(clear_scr_seq, end='') 
 
-def move_cursor(x: int, y: int) -> None:
-    print(f"\033c[{x};{y}f") #! checar ordem / portabilidade
+def highlight(s: str):
+    return "\033[7m" + s + "\033[0m"
 
 def render_msg(msg: dict, username=None) -> str:
     ts, author, recipient, content = msg_to_tuple(msg)
@@ -42,25 +42,32 @@ def draw_scr(title:  str='',
              prompt: str='>',
              user:   str|None=None) -> None:
     sz = get_terminal_size()
-    max_msgs = sz.lines - len(lines) - 3#! descobrir esse 3 com código
 
-    #! não printar o título/extra se tiverem vazios, aproveitar pra contar linhas extras
+    _title  = title[:sz.columns]
+    msgs_   = msgs[-sz.lines:] 
+    _lines  = lines[:sz.lines]
+    _extra  = extra[:sz.columns]
+    _prompt = prompt[:sz.columns]
+    
+    sz_rest = bool(title) + bool(extra) + bool(prompt)
+    max_msgs = sz.lines - len(_lines) - sz_rest
 
-    print(title[:sz.columns]) #! não vai aparecer se tiver mensagens longas
+    _msgs = msgs_[-max_msgs:];    
 
-    for m in msgs[-max_msgs:]: 
+    if _title: print(_title)
+
+    for m in _msgs: 
         print(render_msg(m, username=user)[:sz.columns])
-    for l in lines[:sz.lines]:
+    for l in _lines:
         print(l[:sz.columns])
 
-    print(extra[:sz.columns])
-
-    print(prompt[:sz.columns], end=' ', flush=True)
+    if _extra: print(_extra)
+    print(_prompt, end=' ', flush=True)
 
 def warn(msg: str):
     draw_scr(extra=msg, prompt='')
 
-def input(prompt='', lines=[], title='', clear=True, queue=None):
+def input(prompt=' ', lines=[], title='', clear=True, queue=None):
     self = input
 
     if clear: clear_scr()
