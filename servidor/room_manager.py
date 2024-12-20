2e6159682cd893c440709d2e390506d801278299
theo-al <emailtheoprauerj@gmail.com>
2024-12-20 01:12:00 -0300

    reorganizado room_manager + timer realmente funciona

--- servidor/room_manager.py
+++ servidor/room_manager.py
@@ -3,6 +3,7 @@ from typing import NamedTuple
 
 from threading import Timer
 
+
 room = NamedTuple('room', [('users',    dict[str, dict]),
                            ('messages', list[dict]),
                            ('timer',    Timer | None)])
@@ -11,89 +12,102 @@ rooms = dict[str, room]()
 
 MAX_INACTIVE_TIME = 5*60
 
-def curr_time_str():
+
+def curr_time() -> str:
     return strftime("%Y-%m-%d %H:%M:%S")
 
-def create_room(room_name) -> tuple[bool, str]:
-    if room_name in rooms: return False, "room"
+def get_all_users() -> set[str]: #! deve ser devagar
+    return {"SERVIDOR"}.union(*(r.users.keys() for r in rooms.values()))
+
+def user_in_room(username: str, room_name: str) -> bool: #! deve ser devagar
+    return (username in rooms[room_name].users.keys()) or \
+           (username == "SERVIDOR")
+
+def room_exists(room_name: str) -> bool:
+    return room_name in rooms
+
+def user_exists(username: str) -> bool:
+    return username in get_all_users()
+
+def get_past_messages(room_name: str, max_msgs=50):
+    result = [msg for msg in rooms[room_name].messages
+                  if msg["to"] is None]
+    return result[-max_msgs:] # retorna as últimas {max_msgs} mensagens
+
+
+def list_rooms() -> list[str]:
+    return list(rooms.keys())
+
+def list_users(room_name: str) -> list[str]:
+    return list(rooms[room_name].users.keys()) \
+           if room_name in rooms else []
+
+
+def create_room(room_name: str) -> tuple[bool, str]:
+    if room_exists(room_name): return False, "room"
 
     rooms[room_name] = room(users=dict(),
                             messages=[{
                                 "from": "SERVIDOR", "to": None,
                                 "content": f"bem vindo à sala {room_name}",
-                                "timestamp": curr_time_str(),
+                                "timestamp": curr_time(),
                             }], 
                             timer=None)
     return True, ""
 
-def get_all_users(): #! deve ser devagar
-    return {"SERVIDOR"}.union(*(r.users.keys() for r in rooms.values()))
+def join_room(username: str, room_name: str):
+    if not room_exists(room_name): return False, "room"
+    if     user_exists(username):  return False, "user"
 
-def join_room(username, room_name):
-    if room_name not in rooms:
-        return False, "room"
-    if username in get_all_users():
-        return False, "user"
+    add_message(room_name, "SERVIDOR", f"{username} entrou na sala") #!
 
     if rooms[room_name].timer:
         rooms[room_name].timer.cancel()
-        rooms[room_name].timer = None
+        rooms[room_name] = \
+        rooms[room_name]._replace(timer=None)
 
     rooms[room_name].users[username] = len(rooms[room_name].messages) - 1
     return True, get_past_messages(room_name)
 
-def leave_room(username, room_name):
-    if room_name in rooms:
+def leave_room(username: str, room_name: str):
+    if room_exists(room_name) and user_exists(username):
         rooms[room_name].users.pop(username)
         if not rooms[room_name].users:
-            timer = Timer(MAX_INACTIVE_TIME, lambda: rooms.pop(room_name))
-            rooms[room_name].timer = timer; timer.start()
+            timer = Timer(MAX_INACTIVE_TIME,
+                          lambda: rooms.pop(room_name))
+            rooms[room_name] = \
+            rooms[room_name]._replace(timer=timer)
 
-def list_rooms():
-    return list(rooms.keys())
+            timer.start()
 
-def list_users(room_name):
-    return list(rooms[room_name].users.keys()) \
-           if room_name in rooms else []
-
-def add_message(room_name, username, content, recipient=None):
-    if (room_name not in rooms) or \
-       (username  not in rooms[room_name].users.keys()): return None #! lidar com isso direito
+def add_message(room_name: str, username: str, content: str, recipient=None):
+    if (not room_exists(room_name)) or \
+       (not user_in_room(username, room_name)): return False #! lidar com isso direito
+    #! retornar tipo de erro
 
     msg = {
         #! não tou escrevendo se é unicast ou broadcast (mas é redundante)
         "from": username, "to": recipient,
         "content": content,
-        "timestamp": curr_time_str(),
+        "timestamp": curr_time(),
     }
 
     rooms[room_name].messages.append(msg)
-    return True #! retornar False se não for válido de alguma forma
-    #! checar se tem requisito sobre isso no enunciado
+    return True
 
-def get_messages(room_name, recipient):
-    #! if recipient not in room[room_name].users.keys(): return [] #! lidar com isso direito
+def get_messages(room_name: str, recipient: str):
+    #! if not user_in_room(recipient, room_name): return [] #! lidar com isso direito
 
     last_msg = rooms[room_name].users[recipient]
     next_idx = last_msg + 1
 
     msgs = list(filter(lambda msg: (msg["to"]   == recipient or
                                     msg['from'] == recipient or
-                                    msg["to"] is None),
+                                    msg["to"]   is None),
                 rooms[room_name].messages[next_idx:])) #! checar se duplica
     
     if msgs: rooms[room_name].users[recipient] = len(rooms[room_name].messages) - 1
 
     return msgs
 
-def get_past_messages(room_name, max_msgs=50):
-    result = [msg for msg in rooms[room_name].messages
-                  if msg["to"] is None]
-    #! usar filter
-    return result[-max_msgs:] # Retorna as últimas {max_msgs} mensagens
-
-
-def dump_messages():
-    ... #! colocar num 'nome.db' em json ou pickle
-def load_messages():
-    ... #! puxar de um 'nome.db' em json ou pickle
+#! x._replace(...) -> replace(x, ...)
